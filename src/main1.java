300e1d8fd6bf62b9786682917350c8629282edd3
abdullahgurel <ag39@aof.anadolu.edu.tr>
2022-06-20 01:42:58 +0300

    java for ile ortalama hesaplama

---
+++ src/main1.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class main1 {
+    public static void main(String[] args) {
+
+        /*
+        Java döngüler ile kullanıcının girdiği sayıya kadar çift olan sayıları bulan programı yazıyoruz.
+            Ödev
+        Java döngüler ile 0'dan girilen sayıya kadar olan sayılardan 3 ve 4'e
+        tam bölünen sayıların ortalamasını hesaplayan programı yazınız.
+         */
+
+        Scanner input = new Scanner(System.in);
+
+        int sayi = 0, sayilar = 0, toplam = 0;
+        System.out.print("Bir sayı gir:");
+        sayi = input.nextInt();
+
+        for (int i = 1; i < sayi; i++) {
+
+            if (i % 3 == 0 && i % 4 == 0) {
+                sayilar += i;
+                toplam++;
+
+
+
+
+            }
+        }
+
+         System.out.println(" sayinin ortalamasi : " + (sayilar / toplam));
+
+
+
+    }
+}
+
+
+
